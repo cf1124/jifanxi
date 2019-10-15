7a73ee11d901d76f140226f57255bbc0782bb826
cf1124 <25305251@qq.com>
2019-10-16 00:36:43 +0800

    zhe

--- gulpfile.js
+++ gulpfile.js
@@ -12,27 +12,27 @@ gulp.task("watchall",async()=>{
     // 监听html.进行复制
     gulp.watch("*.html",async()=>{
         gulp.src("*.html")
-        .pipe(gulp.dest("D:\\phpStudy\\WWW\\jifanxifabu"))
+        .pipe(gulp.dest("E:\\phpStudy\\WWW\\jifanxifabu"))
 	});
     // 监听sass文件
     gulp.watch("sass/**/*",async()=>{
         gulp.src("sass/**/*")
         .pipe(sass())
         .pipe(minfyCSS())
-        .pipe(gulp.dest("D:\\phpStudy\\WWW\\jifanxifabu\\css"))
+        .pipe(gulp.dest("E:\\phpStudy\\WWW\\jifanxifabu\\css"))
     })
     //监听js
      gulp.watch("js/**/*",async()=>{
         gulp.src("js/**/*")
 //      .pipe(concat("js/**/*"))
 //      .pipe(uglify())
-        .pipe(gulp.dest("D:\\phpStudy\\WWW\\jifanxifabu\\js"))
+        .pipe(gulp.dest("E:\\phpStudy\\WWW\\jifanxifabu\\js"))
     })
     //监听img
      gulp.watch("img/**/*",async()=>{
         gulp.src("img/**/*")
         .pipe(imagemin())
-        .pipe(gulp.dest("D:\\phpStudy\\WWW\\jifanxifabu\\img"))
+        .pipe(gulp.dest("E:\phpStudy\WWW\\jifanxifabu\\img"))
     })
 })
 
